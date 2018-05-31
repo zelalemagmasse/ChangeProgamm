11f8dc905ace7d508662c6fed098b0d9486b466d
Tofik Mussa <tofahub@gmail.com>
2018-05-31 14:36:50 -0400

    second commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int x, y, z;
+
+        x = 5;
+        y = 9;
+
+        z = x * y;
+
+        System.out.println("The product is " + z);
+    }
+}
